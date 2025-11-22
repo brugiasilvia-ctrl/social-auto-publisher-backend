be9352179003093815346e7a4183ec46d4540c7a
brugiasilvia-ctrl <brugiasilvia@gmail.com>
2025-11-22 11:02:18 +0100

    Add Netlify function to return greeting message

---
+++ netlify/functions/hello.js
@@ -0,0 +1,9 @@
+// netlify/functions/hello.js
+
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: "Ciao dal backend Netlify!" }),
+  };
+};
